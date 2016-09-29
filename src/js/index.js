27816f79503c29e920c56bb29883ac34bed2b8f5
zerob13 <i@zerob13.in>
2016-09-29 15:08:07 +0800

    NimGame

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
-import reverseString from './ReverseString';
+import canWin from './NimGame';
 
-console.dir(reverseString('hello'));
+console.dir(canWin(1));
+console.dir(canWin(2));
+console.dir(canWin(3));
+console.dir(canWin(4));
+console.dir(canWin(10));
